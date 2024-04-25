0fc2e1f8e2f849c84540c9f3d7e4ce8f9fe5f295
furuyashikiiori <s2322057@stu.musashino-u.ac.jp>
2024-04-25 11:49:15 +0900

    サーバー起動コード

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -51,6 +52,16 @@ func realMain() error {
 	mux := router.NewRouter(todoDB)
 
 	// TODO: サーバーをlistenする
+
+	srv:=&http.Server{
+		Addr:port,
+		Handler:mux,
+	}
+
+	err = srv.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return err
+	}
 
 	return nil
 }
